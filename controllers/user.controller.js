15c79e757c7148e2c4a32199fb318becaad9eea8
E <1548-e@mail.net>
2019-08-09 00:13:23 -0500

    testt

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,19 +1,9 @@
-/**
- * Created by Voltron on 12/05/2017.
- */
-// const validator = require('validator');
-const downloadModel = require('../models/user.model');
-// const crypto = require('../util/crypto.util');
-// const userEntity = require('../entities/user.entity');
-// const enums = require('../util/enum.util');
-// const email = require('../util/email.util');
+
 const responseutil = require('../config/response.util');
-// const jwt = require('jsonwebtoken');
-// const SuperSecret = require('../config/SuperSecret');
-// const jwtutil = require('../util/jwt.util');
+const enums = require('../config/enum.util');
+const userModel = require('../models/job.model');
 module.exports = {
     Login: function (req, res, next) {
-
         let objUser = {
             "_id": "",
             "status_item": "",
@@ -26,8 +16,7 @@ module.exports = {
             "description": "",
             "imgurl": "https://www.emojirequest.com/images/SalutingEmoji.jpg"
         };
-        
-        downloadModel.asyncLogin(objUser).then(resolve => {
+        userModel.asyncLogin(objUser).then(resolve => {
             responseutil.Send(res, resolve.statusCode,resolve.result);
             next();
         }, reject => {
@@ -35,23 +24,90 @@ module.exports = {
             next();
         });
     },
-    Create: function (req, res, next) { },
-    Read: function (req, res, next) {
-        //     res.status(200).send({
-        //         'message': 'Welcome product!.'
-        //     });
+    Create: function (req, res, next) {
+        // Fill Inventory Object
+        const datetmp = enums.DateTimeNowToMilliSeconds();
+
+        let job = {
+            status_item: enums.STATUS_ITEM.ACTIVO,
+            create_date: datetmp,
+            modification_date: 0,
+            maker: req.body.maker,
+            name: req.body.name,
+            path: req.body.path
+        }
+
+        userModel.asyncCreate(job).then(resolve => {
+            responseutil.Send(res, resolve.statusCode, resolve.result);
+            next();
+        }, reject => {
+            responseutil.Send(res, reject.statusCode, reject.result, reject.message);
+            next();
+        });
     },
-    Update: function (req, res, next) { },
-    Delete: function (req, res, next) { },
+    Read: function (req, res, next) {
+        const job = { id: req.query.id }
+        userModel.asyncRead(job).then(resolve => {
+            responseutil.Send(res, resolve.statusCode, resolve.result);
+            // next();
+        }, reject => {
+            responseutil.Send(res, reject.statusCode, reject.result, resolve.message);
+            next();
+        });
+    },
+    Update: function (req, res, next) {
+        // Fill Inventory Object
+        const datetmp = enums.DateTimeNowToMilliSeconds();
+
+        let job = {
+            id: req.params.id,
+            status_item: req.body.status_item,
+            modification_date: datetmp,
+            maker: req.body.maker,
+            name: req.body.name,
+            imgurl: req.body.imgurl,
+            description: req.body.description,
+            price: req.body.price,
+            offerprice: req.body.offerprice,
+            start_date: req.body.start_date,
+            end_date: req.body.end_date
+        }
+
+        userModel.asyncUpdate(job).then(resolve => {
+            responseutil.Send(res, resolve.statusCode, resolve.result);
+            next();
+        }, reject => {
+            responseutil.Send(res, reject.statusCode, reject.result, resolve.message);
+            next();
+        });
+
+    },
+    Delete: function (req, res, next) {
+        const datetmp = enums.DateTimeNowToMilliSeconds();
+        let job = {
+            id: req.params.id,
+            status_item: enums.STATUS_ITEM.DELETE,
+            modification_date: datetmp,
+            maker: req.body.maker
+        }
+        userModel.asyncDelete(job).then(resolve => {
+            responseutil.Send(res, resolve.statusCode, resolve.result, resolve.message);
+            next();
+        }, reject => {
+            responseutil.Send(res, reject.statusCode, reject.result, resolve.message);
+            next();
+        });
+    },
     GetAll: function (req, res, next) {
 
-        // downloadModel.asyncGetAll().then(resolve => {
-        //     responseutil.Send(res, resolve.statusCode, resolve.result, resolve.message, resolve.href, resolve.function);
-        //     next();
-        // }, reject => {
-        //     responseutil.Send(res, reject.statusCode, reject.result, reject.message, reject.href, reject.function);
-        //     next();
-        // });
+        let job = { status_item: req.params.status }
+        userModel.asyncGetAll(job).then(resolve => {
+            responseutil.Send(res, resolve.statusCode, resolve.result);
+            //  next();
+        }, reject => {
+            responseutil.Send(res, reject.statusCode, reject.result, reject.message);
+            next();
+        });
     },
 
 }
